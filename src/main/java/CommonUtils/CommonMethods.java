1ad4e2b590d72d19a4b42563a9300cf49b6259aa
RahulKumar <rahul4automation@gmail.com>
2023-06-27 18:13:18 +0530

    Added common method to click on element

--- src/main/java/CommonUtils/CommonMethods.java
+++ src/main/java/CommonUtils/CommonMethods.java
@@ -1,12 +1,22 @@
 package CommonUtils;
 
+import com.microsoft.playwright.ElementHandle;
 import com.microsoft.playwright.Page;
 
 
 public class CommonMethods extends PlayWrightRunner
 {
-    private static void clickElement(String selector) {
+    private static void clickOnElement(String selector) {
         page.click(selector);
+    }
+
+    private static void clickElement(String selector) {
+        ElementHandle element = page.querySelector(selector);
+        if (element != null) {
+            element.click();
+        } else {
+            System.out.println("Element not found: " + selector);
+        }
     }
 
 
